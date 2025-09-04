e39591b0f1edeee8b80da564371f6ab0efa8df54
Tuan Doan <tuandoan14112003@gmail.com>
2025-09-05 01:51:33 +1000

    test

--- startup.sh
+++ startup.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-gunicorn storebuilder.wsgi:application --bind 0.0.0.0:8080 --workers 3 --timeout 120
+#!/usr/bin/env sh
+set -e
+# Optional: python3 manage.py collectstatic --noinput
+# Optional: [ "$RUN_MIGRATIONS" = "1" ] && python3 manage.py migrate --noinput
+exec gunicorn storebuilder.wsgi:application \
+  --bind 0.0.0.0:${PORT:-8080} \
+  --workers ${WEB_CONCURRENCY:-3} \
+  --timeout 120 \
+  --access-logfile - --error-logfile -
