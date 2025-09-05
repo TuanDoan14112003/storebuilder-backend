2478f2dba316de284fcc8e9e088b92ac3f3667e7
Tuan Doan <tuandoan14112003@gmail.com>
2025-09-05 16:54:03 +1000

    test 4

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -e
+cd /app
+python3 manage.py collectstatic --noinput
 # Optional: [ "$RUN_MIGRATIONS" = "1" ] && python3 manage.py migrate --noinput
 exec gunicorn storebuilder.wsgi:application \
   --bind 0.0.0.0:${PORT:-8080} \
